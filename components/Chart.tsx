140c38d86f9fccd2d96574e6f74747e859ea3a88
David Mihal <david@dmihal.com>
2021-08-03 23:42:08 +0100

    Style chart

--- components/Chart.tsx
+++ components/Chart.tsx
@@ -2,31 +2,32 @@ import React, { useLayoutEffect, useRef } from 'react';
 import { Chart as ChartJS } from 'chart.js';
 import 'chartjs-adapter-luxon';
 import ChartStreaming from 'chartjs-plugin-streaming';
-import { LineController, LineElement, PointElement, LinearScale, CategoryScale } from 'chart.js';
+import {
+  LineController,
+  LineElement,
+  PointElement,
+  LinearScale,
+  CategoryScale,
+  Title,
+  Tooltip,
+  SubTitle,
+} from 'chart.js';
 
 ChartJS.register(ChartStreaming);
-ChartJS.register([LineController, LineElement, PointElement, LinearScale, CategoryScale]);
+ChartJS.register([
+  LineController,
+  LineElement,
+  PointElement,
+  LinearScale,
+  CategoryScale,
+  Title,
+  Tooltip,
+  SubTitle,
+]);
 
-interface ChartProps {
-  startBlock: number;
-}
-
-const Chart: React.FC<ChartProps> = () => {
-  // const recentBlockData = useRef([]);
+const Chart: React.FC = () => {
   const maxBlock = useRef(0);
-
-  const data = useRef({
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [],
-        fill: false,
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgba(255, 99, 132, 0.2)',
-        cubicInterpolationMode: 'monotone' as 'monotone',
-      },
-    ],
-  });
+  const data = useRef([]);
   const canvas = useRef<any>(null);
 
   const onRefresh = async (chart) => {
@@ -37,14 +38,15 @@ const Chart: React.FC<ChartProps> = () => {
       const blockData = json.burnedOnBlock[i];
       if (blockData.block > maxBlock.current) {
         maxBlock.current = blockData.block;
-        data.current.datasets[0].data.push({
+        data.current.push({
           x: blockData.timestamp * 1000,
           y: blockData.burned - json.burnedOnBlock[i - 1].burned,
+          block: blockData.block,
         });
       }
     }
 
-    chart.data.datasets[0].data = data.current.datasets[0].data;
+    chart.data.datasets[0].data = data.current;
     chart.update('quiet');
   };
 
@@ -58,18 +60,53 @@ const Chart: React.FC<ChartProps> = () => {
           delay: 2000,
           onRefresh: onRefresh,
         },
+        ticks: {
+          font: {
+            size: 18,
+          },
+        },
       },
       y: {
         title: {
           display: false,
         },
+        ticks: {
+          display: false,
+        },
       },
     },
-    title: {
-      display: true,
-      text: 'ETH burned per block',
-      font: {
-        size: 18,
+    plugins: {
+      title: {
+        display: true,
+        text: 'ETH burned per block',
+        font: {
+          size: 18,
+        },
+      },
+      tooltip: {
+        displayColors: false,
+        titleFont: {
+          size: 18,
+        },
+        bodyFont: {
+          size: 14,
+        },
+        callbacks: {
+          title(ctx: any) {
+            // console.log(ctx);
+            return ctx[0].raw ? `Block ${ctx[0].raw.block.toLocaleString()}` : '';
+          },
+          label(ctx: any) {
+            // console.log(ctx);
+            return ctx.parsed.y.toLocaleString('en-us', {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 14,
+            });
+          },
+          afterLabel() {
+            return 'ETH burned';
+          },
+        },
       },
     },
     interaction: {
@@ -83,10 +120,11 @@ const Chart: React.FC<ChartProps> = () => {
       data: {
         datasets: [
           {
-            label: '# of Votes',
+            label: 'ETH burned per block',
             data: [],
             fill: false,
-            backgroundColor: 'rgb(255, 99, 132)',
+            borderWidth: 6,
+            backgroundColor: '#FC940B',
             borderColor: 'rgba(255, 99, 132, 0.2)',
             cubicInterpolationMode: 'monotone' as 'monotone',
           },
