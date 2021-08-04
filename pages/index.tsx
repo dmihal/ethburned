be7188851e77637dc5686a68ecd2c0b8f51c24fc
spacerent-cto <hello@kallemoen.com>
2021-08-04 16:43:44 +0100

    Added new CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,12 +57,12 @@ export const Home: NextPage<HomeProps> = ({ total, yesterday, block }) => {
       <div className="row">
         <div className="card">
           <div className="big">{decimal(data.total)} ETH</div>
-          <div>Total burned</div>
+          <div className="sub-text">Total burned</div>
         </div>
 
         <div className="card">
           <div className="big">{decimal(data.yesterday)} ETH</div>
-          <div>Burned in the last hour</div>
+          <div className="sub-text">Burned in the last hour</div>
         </div>
       </div>
 
@@ -73,12 +73,13 @@ export const Home: NextPage<HomeProps> = ({ total, yesterday, block }) => {
       <style jsx>{`
         main {
           width: 100%;
-          padding: 2rem 0 3rem;
+          padding: 2rem 1rem 3rem 1rem;
           flex: 1;
           display: flex;
           flex-direction: column;
           justify-content: center;
           align-items: center;
+          max-width: 750px;
         }
 
         .title {
@@ -102,15 +103,31 @@ export const Home: NextPage<HomeProps> = ({ total, yesterday, block }) => {
 
         .row {
           display: flex;
+          width: 100%;
+          margin: 2em 0em;
         }
 
         .card {
           text-align: center;
-          margin: 8px;
-          padding: 8px;
+          padding: 15px 10px;
           border: solid 1px #d0d1d9;
           border-radius: 8px;
           font-size: 24px;
+          width: 100%;
+        }
+
+        .card:first-child {
+          margin-right: 10px;
+        }
+
+        .sub-text {
+          color: #7a8190;
+        }
+
+        .small {
+          font-size: 14px;
+          margin-top: 4px;
+          color: #333;
         }
 
         .block-num {
@@ -122,11 +139,21 @@ export const Home: NextPage<HomeProps> = ({ total, yesterday, block }) => {
 
         .big {
           font-size: 32px;
+          margin-bottom: 5px;
+        }
+
+        .event-headline {
+          font-size: 24px;
+          line-height: 1.2;
         }
 
         @media (max-width: 600px) {
           .row {
             flex-direction: column;
+          }
+          .card:first-child {
+            margin-right: 0px;
+            margin-bottom: 10px;
           }
         }
       `}</style>
