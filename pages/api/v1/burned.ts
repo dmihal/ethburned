6139ab7fc344a5dffe81fea1d9813e9a7b8039e3
David Mihal <david@dmihal.com>
2021-10-10 19:16:11 +0100

    CORS headers

--- pages/api/v1/burned.ts
+++ pages/api/v1/burned.ts
@@ -7,6 +7,16 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     { burned: yesterday, burnedUSD: yesterdayUSD },
   ] = await Promise.all([getTotalBurned(), getBurnedLastHr()]);
 
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method == 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+
   res.setHeader('Cache-Control', 'max-age=0, s-maxage=5, stale-while-revalidate');
   res.json({
     success: true,
--- pages/api/v1/recently-burned/[period].ts
+++ pages/api/v1/recently-burned/[period].ts
@@ -12,6 +12,17 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
   const cache = cacheTime[req.query.period.toString()];
   res.setHeader('Cache-Control', `max-age=0, s-maxage=${cache}, stale-while-revalidate`);
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method == 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+
   res.json({
     success: true,
     burned,
